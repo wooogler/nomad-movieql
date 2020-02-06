64b459789d1b449ba32aa6d087d1de1d53af45d2
wooogler <leesang627@gmail.com>
2020-02-06 20:05:40 +0900

    movieql server

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const {GraphQLServer} = require('graphql-yoga');
+import {GraphQLServer} from 'graphql-yoga';
+import resolvers from './graphql/resolvers';
+
 
 const server = new GraphQLServer({
-  
+  typeDefs: 'graphql/schema.graphql',
+  resolvers
 });
 
 server.start(() => console.log('GraphQL Server Running'));
